f85fccd1e1836f56c287c35049e605e0b469d9e5
chris-diesner <chr.diesner@mailbox.org>
2025-02-04 21:22:50 +0100

    fix app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/Homepage";
+import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import "./App.css";
 function App() {
