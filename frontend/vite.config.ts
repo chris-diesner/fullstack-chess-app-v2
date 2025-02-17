fb9e3c1930fdac9613d13fa9d7b2e0fc7ca73cfe
chris-diesner <chr.diesner@mailbox.org>
2025-02-17 13:09:54 +0100

    fixed fastAPI routing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  // `globalThis.process` statt `process`
+  globalThis.process = globalThis.process || { env: {} };
+
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    define: {
+      'import.meta.env.VITE_BACKEND_URL': JSON.stringify(env.VITE_BACKEND_URL),
+    },
+  };
+});
